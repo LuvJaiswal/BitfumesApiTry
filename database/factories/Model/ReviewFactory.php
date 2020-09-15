774f1fb7541698dab7bebbf058a2117a0521ee7f
Luv Jaiswal <42119313+LuvJaiswal@users.noreply.github.com>
2020-09-15 20:59:49 +0545

    model class made

---
+++ database/factories/Model/ReviewFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories\Model;
+
+use App\Models\Model\Review;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class ReviewFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Review::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
