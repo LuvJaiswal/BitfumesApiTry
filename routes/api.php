287127a3b5802ad5ec5301a22d159f4c65ed641b
Luv Jaiswal <42119313+LuvJaiswal@users.noreply.github.com>
2020-09-19 11:58:57 +0545

    Product controller target classs error solved

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,12 @@
 <?php
-
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ReviewController;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-
-Route::apiResource('/products','ProductController');
-
+Route::apiResource('/products',ProductController::class);
 Route::group(['prefix'=>'products'],function(){
-    Route::apiResource('/{product}/reviews','ReviewController');
+	Route::apiResource('/{product}/reviews',ReviewController::class);
 });
